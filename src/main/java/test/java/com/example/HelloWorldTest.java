c23d23c58b0865af090cb233ceb442907f8d1e17
souaibou18 <163294336+souaibou18@users.noreply.github.com>
2025-10-27 15:06:25 +0100

    Add HelloWorld class with main and add methods

---
+++ src/main/java/test/java/com/example/HelloWorldTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, SonarCloud!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
